267aa41c14c1b4218a5edaa632f37e7a23af69ba
samuelalonsogil <asbarxas.samuelalonso@gmail.com>
2022-02-23 23:36:09 +0100

    ejercicio shakespeare

--- 2aEv/PreExamen/FragmentCommunicationShakespeare/app/src/main/java/com/example/fragmentcommunicationshakespeare/DetailFragment.java
+++ 2aEv/PreExamen/FragmentCommunicationShakespeare/app/src/main/java/com/example/fragmentcommunicationshakespeare/DetailFragment.java
@@ -1,4 +1,4 @@
-package com.example.fragmentsexamenanhopasado;
+package com.example.fragmentcommunicationshakespeare;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,28 +10,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-
 public class DetailFragment extends Fragment {
-    public static String KEY_TITLE = "key_title";
-    Bundle bundle;
+    public static final String KEY_TITLE = "KEY_TITLE";
+    public Bundle bundle;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate( R.layout.detail_fragment, container, false );
+        return inflater.inflate(R.layout.fragment_detail, container, false);
     }
-
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         bundle = getArguments();
-        if ( bundle != null && bundle.containsKey( KEY_TITLE ) )
-            showSelectText( bundle.getString( KEY_TITLE ) ) ;
+        if (bundle!=null && bundle.containsKey(KEY_TITLE) ) showSelectedText( bundle.getString(KEY_TITLE) );
     }
 
-
-    public void showSelectText(String texto){
-        ( (TextView) requireView().findViewById(R.id.muchoTexto) ).setText(texto);
+    public void showSelectedText(String text) {
+        ( (TextView) requireView().findViewById(R.id.textView01) ).setText( text );
     }
 }
