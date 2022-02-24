0bd4c8bf86f87dfa784c9b7610446f73689874f7
samuelalonsogil <asbarxas.samuelalonso@gmail.com>
2022-02-24 10:19:16 +0100

    ns

--- 2aEv/JuegoPepe/app/src/main/java/com/example/juegopepe/Animadora.java
+++ 2aEv/JuegoPepe/app/src/main/java/com/example/juegopepe/Animadora.java
@@ -1,2 +1,18 @@
-package com.example.juegopepe;public class Animadora {
+package com.example.juegopepe;
+
+import android.graphics.Rect;
+
+public class Animadora {
+    private Rect rect;
+    private int cuentaFrame;
+    private int actualFrame;
+    private long corazonFrame;
+    private int periodoFrame;
+    private int anchoFrame;
+
+    public Animadora(float alturaFrame, float anchoFrame, int cuentaFrame) {
+        this.cuentaFrame = cuentaFrame;
+        this.actualFrame = actualFrame;
+        this.anchoFrame = anchoFrame;
+    }
 }
